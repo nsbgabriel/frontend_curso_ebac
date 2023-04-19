a92090561f7da68f02f6099bea812affc8c9a7ce
nsbgabriel <108702727+nsbgabriel@users.noreply.github.com>
2023-04-19 09:06:18 -0300

    Add files via upload

---
+++ exercicioM26.ts
@@ -0,0 +1,9 @@
+function multiplicar(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+const resultado = multiplicar(2, 3);
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+const dizOla = saudacao("Gabriel");
